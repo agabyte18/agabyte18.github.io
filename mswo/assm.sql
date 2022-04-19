f2c5c1caf4f442a463699f7f998de374af4506a4
AGABA, Philip <agabyte18@gmail.com>
2022-04-19 01:43:25 +0100

    update assm.sql

--- mswo/assm.sql
+++ mswo/assm.sql
@@ -1,4 +1,5 @@
 DECLARE
+v_object_owner varchar2(50) := upper('&object_owner');
 v_object_name varchar2(50) := upper('&object_name');
 v_object_type varchar2(50) := upper('&object_type');
 v_unformatted_blocks number;
@@ -14,7 +15,7 @@ v_fs4_bytes number;
 v_full_blocks number;
 v_full_bytes number;
 BEGIN
-dbms_space.space_usage ('HR', v_object_name, v_object_type, v_unformatted_blocks,
+dbms_space.space_usage (v_object_owner, v_object_name, v_object_type, v_unformatted_blocks,
                           v_unformatted_bytes, v_fs1_blocks, v_fs1_bytes,
                           v_fs2_blocks, v_fs2_bytes, v_fs3_blocks, v_fs3_bytes,
                           v_fs4_blocks, v_fs4_bytes, v_full_blocks, v_full_bytes);
