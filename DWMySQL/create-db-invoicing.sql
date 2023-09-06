51c8018f683ccc7fc1294d94d341640d441f82f4
AGABA, Philip <agabyte18@gmail.com>
2023-09-06 08:32:41 +0100

    Developing with MySQL

---
+++ DWMySQL/create-db-invoicing.sql
@@ -0,0 +1,86 @@
+DROP DATABASE IF EXISTS `DWM_invoicing`;
+CREATE DATABASE `DWM_invoicing`; 
+USE `DWM_invoicing`;
+
+SET NAMES utf8mb4 ;
+SET character_set_client = utf8mb4 ;
+
+CREATE TABLE `payment_methods` (
+  `payment_method_id` tinyint NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`payment_method_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+INSERT INTO `payment_methods` VALUES (1,'Cash');
+INSERT INTO `payment_methods` VALUES (2,'PayPal');
+INSERT INTO `payment_methods` VALUES (3,'Credit Card');
+INSERT INTO `payment_methods` VALUES (4,'Wire Transfer');
+
+CREATE TABLE `clients` (
+  `client_id` int NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `state` char(2) NOT NULL,
+  `phone` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+INSERT INTO `clients` VALUES (1,'Vite','8 Nevada Avenue','Syracuse','NY','315-252-8416');
+INSERT INTO `clients` VALUES (2,'HopWorks','23156 Glendale Avenue','Huntington','WV','304-659-2218');
+INSERT INTO `clients` VALUES (3,'Citadel','017 Pawling Street','San Francisco','CA','415-144-7147');
+INSERT INTO `clients` VALUES (4,'Viddy','81674 Chesterfield Lane','Waco','TX','254-750-2902');
+INSERT INTO `clients` VALUES (5,'TitleCouch','0777 Farmco Circle','Portland','OR','971-888-0368');
+
+CREATE TABLE `invoices` (
+  `invoice_id` int NOT NULL,
+  `number` varchar(50) NOT NULL,
+  `client_id` int NOT NULL,
+  `invoice_total` decimal(9,2) NOT NULL,
+  `payment_total` decimal(9,2) NOT NULL DEFAULT '0.00',
+  `invoice_date` date NOT NULL,
+  `due_date` date NOT NULL,
+  `payment_date` date DEFAULT NULL,
+  PRIMARY KEY (`invoice_id`),
+  KEY `FK_client_id` (`client_id`),
+  CONSTRAINT `FK_client_id` FOREIGN KEY (`client_id`) REFERENCES `clients` (`client_id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+INSERT INTO `invoices` VALUES (1,'02-538-3396',2,101.79,0.00,'2019-03-09','2019-03-29',NULL);
+INSERT INTO `invoices` VALUES (2,'03-898-7846',5,175.32,8.18,'2019-06-11','2019-07-01','2019-02-12');
+INSERT INTO `invoices` VALUES (3,'20-339-0335',5,147.99,0.00,'2019-07-31','2019-08-20',NULL);
+INSERT INTO `invoices` VALUES (4,'56-934-9581',3,152.21,0.00,'2019-03-08','2019-03-28',NULL);
+INSERT INTO `invoices` VALUES (5,'87-052-2324',5,169.36,0.00,'2019-07-18','2019-08-07',NULL);
+INSERT INTO `invoices` VALUES (6,'75-699-6626',1,157.78,74.55,'2019-01-29','2019-02-18','2019-01-03');
+INSERT INTO `invoices` VALUES (7,'68-104-9863',3,133.87,0.00,'2019-09-04','2019-09-24',NULL);
+INSERT INTO `invoices` VALUES (8,'78-562-1373',1,189.12,0.00,'2019-05-20','2019-06-09',NULL);
+INSERT INTO `invoices` VALUES (9,'55-403-1181',5,172.17,0.00,'2019-07-09','2019-07-29',NULL);
+INSERT INTO `invoices` VALUES (10,'78-276-1777',1,159.50,0.00,'2019-06-30','2019-07-20',NULL);
+INSERT INTO `invoices` VALUES (11,'26-868-0661',3,126.15,0.03,'2019-01-07','2019-01-27','2019-01-11');
+INSERT INTO `invoices` VALUES (13,'42-266-1022',5,135.01,87.44,'2019-06-25','2019-07-15','2019-01-26');
+INSERT INTO `invoices` VALUES (15,'88-185-9805',3,167.29,80.31,'2019-11-25','2019-12-15','2019-01-15');
+INSERT INTO `invoices` VALUES (16,'11-411-8124',1,162.02,0.00,'2019-03-30','2019-04-19',NULL);
+INSERT INTO `invoices` VALUES (17,'39-615-9694',3,126.38,68.10,'2019-07-30','2019-08-19','2019-01-15');
+INSERT INTO `invoices` VALUES (18,'53-269-4403',5,180.17,42.77,'2019-05-23','2019-06-12','2019-01-08');
+INSERT INTO `invoices` VALUES (19,'83-549-2205',1,134.47,0.00,'2019-11-23','2019-12-13',NULL);
+
+CREATE TABLE `payments` (
+  `payment_id` int NOT NULL AUTO_INCREMENT,
+  `client_id` int NOT NULL,
+  `invoice_id` int NOT NULL,
+  `date` date NOT NULL,
+  `amount` decimal(9,2) NOT NULL,
+  `payment_method` tinyint NOT NULL,
+  PRIMARY KEY (`payment_id`),
+  KEY `fk_client_id_idx` (`client_id`),
+  KEY `fk_invoice_id_idx` (`invoice_id`),
+  KEY `fk_payment_payment_method_idx` (`payment_method`),
+  CONSTRAINT `fk_payment_client` FOREIGN KEY (`client_id`) REFERENCES `clients` (`client_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_payment_invoice` FOREIGN KEY (`invoice_id`) REFERENCES `invoices` (`invoice_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_payment_payment_method` FOREIGN KEY (`payment_method`) REFERENCES `payment_methods` (`payment_method_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+INSERT INTO `payments` VALUES (1,5,2,'2019-02-12',8.18,1);
+INSERT INTO `payments` VALUES (2,1,6,'2019-01-03',74.55,1);
+INSERT INTO `payments` VALUES (3,3,11,'2019-01-11',0.03,1);
+INSERT INTO `payments` VALUES (4,5,13,'2019-01-26',87.44,1);
+INSERT INTO `payments` VALUES (5,3,15,'2019-01-15',80.31,1);
+INSERT INTO `payments` VALUES (6,3,17,'2019-01-15',68.10,1);
+INSERT INTO `payments` VALUES (7,5,18,'2019-01-08',32.77,1);
+INSERT INTO `payments` VALUES (8,5,18,'2019-01-08',10.00,2);
