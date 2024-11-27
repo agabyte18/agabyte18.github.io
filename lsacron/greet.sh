c2179aaeabd34c3bc722756afeb8282e16d649b5
AGABA <AGABA>
2024-11-27 03:00:39 -0500

    Add --greet.sh--

--- lsacron/greet.sh
+++ lsacron/greet.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #
 #
-read -p "How are you feeling today? " feeling
 echo
+read -p "*** How are you feeling today? " feeling
 echo
-echo "You're feeling $feeling."
-echo "I sincerely hope you have a great day."
-echo
+echo "*** You're feeling $feeling."
+echo "*** I sincerely hope you have a great day."
 echo
