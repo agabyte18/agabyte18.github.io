ff2bff9c2e65726aab44a89a2fef836b5c578944
AGABYTE <AGABYTE>
2024-12-01 03:19:02 +0100

    Simple Server

--- lsacron/simple-server.js
+++ lsacron/simple-server.js
@@ -1,6 +1,6 @@
 const http = require("http")
 
 http.createServer((req, res)=>{
-  res.write("Welcome to GloTogether")
+  res.write("<h1>GloTogether</h1> <h2>Powered by Node.</h2>")
   res.end()
 }).listen(3000)
